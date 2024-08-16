0d0925ea57b3e45ccbf4d9d6124c1d757fc2cd3f
Prakhar <prakhar102004@gmail.com>
2024-08-16 16:43:11 +0530

    selecting all elements from html

--- dom-extra/main.js
+++ dom-extra/main.js
@@ -1,1 +1,9 @@
 console.log("hi from js");
+
+const title = document.getElementById("title").value;
+const description = document.getElementById("description").value;
+const btn = document.getElementById("add-todo-btn");
+
+console.log({ title, description, btn });
+
+btn.addEventListener("click", function () {});
