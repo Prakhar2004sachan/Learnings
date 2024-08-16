80565b8d9ec86d5592fe21cd61adf66ac50f2a31
Prakhar <prakhar102004@gmail.com>
2024-08-16 17:04:01 +0530

    done with logic

--- dom-extra/main.js
+++ dom-extra/main.js
@@ -1,9 +1,35 @@
 console.log("hi from js");
 
-const title = document.getElementById("title").value;
-const description = document.getElementById("description").value;
 const btn = document.getElementById("add-todo-btn");
 
-console.log({ title, description, btn });
+let globalId = 1;
 
-btn.addEventListener("click", function () {});
+function markAsDone(todoID) {
+  const parent = document.getElementById(todoID);
+  parent.children[2].innerHTML = "Done!";
+}
+
+function createChild(title, description, id) {
+  const child = document.createElement("div");
+  const firstGrandParent = document.createElement("div");
+  firstGrandParent.innerHTML = title;
+  const secondGrandParent = document.createElement("div");
+  secondGrandParent.innerHTML = description;
+  const thirdGrandParent = document.createElement("div");
+  thirdGrandParent.setAttribute("onclick", `markAsDone${id}`);
+  child.appendChild(firstGrandParent);
+  child.appendChild(secondGrandParent);
+  child.appendChild(thirdGrandParent);
+  child.setAttribute("id", id);
+  return child;
+}
+
+const addtodo = function () {
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  const parent = document.getElementById("todos");
+
+  parent.appendChild(createChild(title, description, globalId++));
+};
+
+btn.addEventListener("click", addtodo);
